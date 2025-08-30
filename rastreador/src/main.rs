5158f3b88dba96dc09ec1b2eeb2967fbc6819d54
josh_lis <josh.solisf@gmail.com>
2025-08-30 12:39:26 -0600

    c code to test

--- rastreador/src/main.rs
+++ rastreador/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn wait_for_key() {
+    let mut stdout = io::stdout();
+    let _ = stdout.write_all(b"Press ANY key for next step");
+    let _ = stdout.flush(); 
+
+    let mut stdin = io::stdin();
+    let mut buffer = [0; 1];
+    let _ = stdin.read_exact(&mut buffer);
 }
--- rastreador/src/maximo.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-lista = [3, 1, 4, 1, 5, 9, 2, 6]
-maximo = max(lista)
-print(maximo)
